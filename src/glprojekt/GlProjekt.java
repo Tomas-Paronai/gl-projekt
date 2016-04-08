88a2b99b612cc66aed31db176f543b747a53f258
Tomas-Paronai <tomas.paronai@akademiasovy.sk>
2016-04-08 12:37:08 +0200

    update

--- src/glprojekt/GlProjekt.java
+++ src/glprojekt/GlProjekt.java
@@ -3,7 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package glprojekt;
+
+import glprojekt.api.HandlerDB;
 
 /**
  *
@@ -16,6 +19,6 @@ public class GlProjekt {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+}
     
 }
