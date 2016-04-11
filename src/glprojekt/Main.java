20ed0fc31a5d6211e0a34b1e2d254b13d1746073
bubosam <patrik.patinak@akademiasovy.sk>
2016-04-11 22:53:48 +0200

    Main Screen form

--- src/glprojekt/Main.java
+++ src/glprojekt/Main.java
@@ -7,6 +7,7 @@ package glprojekt;
 
 import glprojekt.gui.Add_employee_1;
 import glprojekt.gui.Delete_employee;
+import glprojekt.gui.Main_screen;
 import glprojekt.gui.Update_employee_1;
 
 /**
@@ -20,14 +21,17 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       Add_employee_1 employ = new Add_employee_1();
-          employ.setVisible(true);
+      /* Add_employee_1 employ = new Add_employee_1();
+          employ.setVisible(true);*/
        
       /*Delete_employee delete = new Delete_employee();
        delete.setVisible(true);
      
        /*Update_employee_1 update = new Update_employee_1();
      update.setVisible(true);*/
+      
+      Main_screen screen = new Main_screen();
+      screen.setVisible(true);
        
           
         
