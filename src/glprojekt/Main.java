ab6bed566d6d8c16da9e4666db0c2042d2661d31
dominikkolesar <dominik.kolesar@akademiasovy.sk>
2016-04-12 16:26:40 +0200

    nic len zmena mainu

--- src/glprojekt/Main.java
+++ src/glprojekt/Main.java
@@ -25,17 +25,17 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-<<<<<<< HEAD
-       /*Add_employee employ = new Add_employee();
-          employ.setVisible(true);*/
+
+       Add_employee employ = new Add_employee();
+          employ.setVisible(true);
           
                  
-      Login login = new Login();
-      login.setVisible(true);
-=======
+      /*Login login = new Login();
+      login.setVisible(true);*/
+
       /* Add_employee_1 employ = new Add_employee_1();
           employ.setVisible(true);*/
->>>>>>> de0e90ca65ef172f0bfee7534f5334b623242829
+
        
       /*Delete_employee delete = new Delete_employee();
        delete.setVisible(true);
@@ -43,8 +43,8 @@ public class Main {
        /*Update_employee_1 update = new Update_employee_1();
      update.setVisible(true);*/
       
-      Main_screen screen = new Main_screen();
-      screen.setVisible(true);
+     /* Main_screen screen = new Main_screen();
+      screen.setVisible(true);*/
        
           
         
